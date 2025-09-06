dffc5ae91d6cc16cd06e8aea446c8f55dd2afe7b
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2025-09-06 18:49:52 +0530

    Complete Basic Loop Patterns

--- BeginnerProjects/Loops.java
+++ BeginnerProjects/Loops.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[]args){
-        //For-loop || Print Number from 1 to 10
+   /*      //For-loop || Print Number from 1 to 10
         int n;
         Scanner Sc= new Scanner(System.in);
         System.out.println("The loop should continue until : ");
@@ -10,6 +10,21 @@ public class Loops {
         for(int i=1 ;i<=n;){
             System.out.println(i++);
         }
-        Sc.close();
+        Sc.close(); */
+
+        //Pattern printing | Basic one 
+        int n,m;
+        char a;
+        Scanner Sc= new Scanner(System.in);
+        a=Sc.nextLine().charAt(0);
+        n=Sc.nextInt();
+        m=Sc.nextInt();
+        for(int i=0 ;i<n;i++){
+            for(int j=0 ; j<m;j++){
+                System.out.print(a);
+            }
+           System.out.println();
+        }
+   
     }
 }
