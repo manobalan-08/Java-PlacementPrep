e314fa56bfd3120803948151ce34712942570300
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2025-09-06 11:24:28 +0530

    Complete Variables

--- BeginnerProjects/Variables.java
+++ BeginnerProjects/Variables.java
@@ -4,12 +4,14 @@ public class Variables{
         double deci=20.01;
         char character='A';
         String Company_name="Anchortrics";
-        
+        long Company_networth=3400000000l;
+        float Company_share=20.01f;
         System.out.println("The Age of the founder is : "+Founder_age);
         System.out.println("The share of the R&D department  is : "+deci);
         System.out.println("The First letter of the Company is :"+character);
         System.out.println("The Company name is :"+Company_name);
-
+        System.out.println("The Company name is :"+Company_networth);
+        System.out.println("The Company name is :"+Company_share);
 
     }
 }
