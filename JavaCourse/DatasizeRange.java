61d2a46094b8f8169b8ca6682f7491ad461722ad
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2025-09-20 12:12:39 +0530

    Create Java Course Directory for Course Practice

---
+++ JavaCourse/DatasizeRange.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+public class DatasizeRange {
+    
+    public static void main(String args[]){
+
+           System.out.println("The Minimum value of Integer is : "+Integer.MIN_VALUE);
+           System.out.println("The Maximum value of the Integer is : "+Integer.MAX_VALUE);
+           System.out.println("The Size of the Integer is : "+Integer.BYTES);
+
+           System.out.println("The Minimum value of Bytes is : "+Byte.MIN_VALUE);
+           System.out.println("The Maximum value of the Bytes is : "+Byte.MAX_VALUE);
+           System.out.println("The Size of the Bytes is : "+Byte.BYTES);
+
+           System.out.println("The Minimum value of Float is : "+Float.MIN_VALUE);
+           System.out.println("The Maximum value of the Float is : "+Float.MAX_VALUE);
+           System.out.println("The Size of the Float is : "+Float.BYTES);
+
+           System.out.println("The Minimum value of Short is : "+Short.MIN_VALUE);
+           System.out.println("The Maximum value of the short is : "+Short.MAX_VALUE);
+           System.out.println("The Size of the short is : "+Short.BYTES);
+           
+    }
+
+}
---
+++ JavaCourse/First.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+public class First {
+     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I know your Name? : ");
+        String name=sc.nextLine();
+        System.out.println("Welcome "+name+" to the Java course provided by  Abdul Bari !");
+     }
+}
